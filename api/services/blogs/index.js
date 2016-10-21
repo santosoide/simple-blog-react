8004db826785e2684d6717622206296889ab3c36
Edi Santoso <edicyber@gmail.com>
2016-10-21 22:04:46 +0700

    find data by slug

--- api/services/blogs/index.js
+++ api/services/blogs/index.js
@@ -5,16 +5,34 @@ import hooks from './hooks';
 export default function blogsService() {
   const app = this;
 
+  const db = new NeDB({
+    filename: `${__dirname}/blogs.nedb`,
+    autoload: true
+  });
+
+  const slugService= {
+    setup(app) {
+      this.app = app
+    },
+
+    find(params) {
+      return new Promise( function( resolve, reject ) {
+        db.findOne({ slug: params.slug }).exec(function(err,doc){
+          resolve(doc);
+        });
+      });
+    }
+  }
+
   app.use('/blogs', feathersNedb({
-    Model: new NeDB({
-      filename: `${__dirname}/blogs.nedb`,
-      autoload: true
-    }),
+    Model: db,
     paginate: {
       default: 25,
       max: 100
     }
   }));
+
+  app.use('/blog/:slug', slugService)
 
   app.service('blogs')
     .before(hooks.before)
